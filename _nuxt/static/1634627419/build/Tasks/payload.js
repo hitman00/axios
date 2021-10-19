2730b58c1b6cbac11cba1066c7d07487cb0063a6
hitman00 <hitman00@users.noreply.github.com>
2021-10-19 07:10:20 +0000

    deploy: 08406154b3bf016f1ef52ccd483841692cfbac0e

---
+++ _nuxt/static/1634627419/build/Tasks/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/build/Tasks", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1634627419/user/5/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmembers-list-design\u002F_nuxt\u002Fstatic\u002F1634627419",layout:"default",error:a,serverRendered:true,routePath:"\u002Fuser\u002F5",config:{_app:{basePath:"\u002Fmembers-list-design\u002F",assetsPath:"\u002Fmembers-list-design\u002F_nuxt\u002F",cdnURL:a}}}}(null));
