209b55a47794054b52f852b7d86628792a3f1a8c
Mehdi HosseinZade <arshen@outlook.com>
2021-10-19 11:05:56 +0330

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
-  mode: 'spa',
   // Global page headers: https://go.nuxtjs.dev/config-head
   router: {
     base: '/members-list-design/',
