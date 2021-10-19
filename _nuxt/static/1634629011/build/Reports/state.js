030be1f4b769b28976f733ce182db71f618b5aaf
hitman00 <hitman00@users.noreply.github.com>
2021-10-19 07:45:38 +0000

    deploy: ff5c1c8fbbfa7c98ad06b03e5771395d3818c4b4

--- _nuxt/static/1634629011/build/Reports/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmembers-list-design\u002F_nuxt\u002Fstatic\u002F1634629011",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbuild\u002FReports",config:{_app:{basePath:"\u002Fmembers-list-design\u002F",assetsPath:"\u002Fmembers-list-design\u002F_nuxt\u002F",cdnURL:a}}}}(null));
